a3fca58a196d8e0bb3ed3487b16b11283c20268b
Vineet P <poojaryvineeet@gmail.com>
2025-10-05 21:43:13 +0530

    feat(0007): base url changes

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,7 @@ const RENDER_BASE_URL = 'https://rest-shop.onrender.com';
 const LOCAL_HOST_BASE_URL = ' http://localhost:8626';
 // Base API instance
 const api = axios.create({
-  baseURL: LOCAL_HOST_BASE_URL,
+  baseURL: RENDER_BASE_URL,
   timeout: 10000, // 10s timeout
   headers: {
     'Content-Type': 'application/json',
