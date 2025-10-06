91cff08349180c9861d382d19e62ee1d1c3af58f
Vineet P <poojaryvineeet@gmail.com>
2025-10-06 23:03:04 +0530

    feat(0007): show toast fix

--- src/utils/common-function.js
+++ src/utils/common-function.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { showToast } from '@/components/showToast';
+import { showToast } from '../components/ShowToast';
 
 export function isFormValid(fields, formData) {
   return fields.every((field) => {
